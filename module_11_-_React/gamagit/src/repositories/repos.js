fd71fbb3ed800f65620b2d3f1b65b22edc90a908
antonio357 <carlosalbuquerque359@gmail.com>
2020-05-31 21:46:02 -0300

    fixing little bug

--- module_11_-_React/gamagit/src/repositories/repos.js
+++ module_11_-_React/gamagit/src/repositories/repos.js
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
 import * as styled from "./styled";
+import {useHistory} from "react-router-dom"
 
 export default function Repos() {
+    const history = useHistory()
     const [repos, setRepos] = useState([]);
     const [reposLi, setReposLi] = useState();
 
     useEffect(() => {
-        JSON.parse(localStorage.getItem("repos")).map((repo) => repos.push(repo))
+        let localRequest = localStorage.getItem("repos") 
+        if (!localRequest) {
+            history.push("/")
+            return
+        }
+        JSON.parse(localRequest).map((repo) => repos.push(repo))
         console.log("Repos -> repos", repos)
         setReposLi(repos.map(repo => {
             return <li> {repo.name} <a href={repo.link}> go to repository </a> </li>
