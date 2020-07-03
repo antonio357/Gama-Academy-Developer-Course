fe8ba990431c65ec4318924a2596b602ee5ab9d0
antonio357 <carlosalbuquerque359@gmail.com>
2020-07-03 19:00:33 -0300

    client-server-model to proced to GraphQL, params, redirect, rendering

--- module14_-_GraphQL/client-server-sample/src/main.js
+++ module14_-_GraphQL/client-server-sample/src/main.js
@@ -1,4 +1,7 @@
 import {createServer} from 'http'
+import {readFile} from 'fs'
+import {resolve} from 'path'
+import {parse} from 'querystring'
 
 const server = createServer((request, response) => {
     switch (request.url) {
@@ -10,6 +13,51 @@ const server = createServer((request, response) => {
                 status: "ok"
             }))
             response.end()
+            break
+        }
+        case '/authenticate': {
+            let data = ''
+            request.on('data', (chunk) => {
+                data += chunk
+            })
+            const params = parse(data)
+            request.on('end', () => {
+                console.log(parse(data))
+                response.writeHead(301, {
+                    Location: '/home'
+                })
+                response.end()
+            })
+            break
+        }
+        case '/sign-in': {
+            const filePath = resolve(__dirname, './pages/sign-in.html')
+            readFile(filePath, (error, file) => {
+                if (error) {
+                    response.writeHead(500, "HTML file read process failed")
+                    response.end()
+                    return
+                }
+
+                response.writeHead(200)
+                response.write(file)
+                response.end()
+            })
+            break
+        }
+        case '/home': {
+            const filePath = resolve(__dirname, './pages/home.html')
+            readFile(filePath, (error, file) => {
+                if (error) {
+                    response.writeHead(500, "HTML file read process failed")
+                    response.end()
+                    return
+                }
+
+                response.writeHead(200)
+                response.write(file)
+                response.end()
+            })
             break
         }
         default: {
