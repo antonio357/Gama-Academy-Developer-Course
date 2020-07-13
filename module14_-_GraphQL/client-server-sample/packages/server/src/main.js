6d53f7953656394bf6f60093e69c1907271bfec5
antonio357 <carlosalbuquerque359@gmail.com>
2020-07-13 17:33:02 -0300

    GraphQL, client-server-sample, using express

--- module14_-_GraphQL/client-server-sample/packages/server/src/main.js
+++ module14_-_GraphQL/client-server-sample/packages/server/src/main.js
@@ -1,37 +1,22 @@
-import {createServer} from 'http'
-import {parse} from 'querystring'
+import express from 'express'
 
-const server = createServer((request, response) => {
-    switch (request.url) {
-        case '/status': {
-            response.writeHead(200, {
-                "Content-Type": "application/json"
-            })
-            response.write(JSON.stringify({
-                status: "ok"
-            }))
-            response.end()
-            break
-        }
-        case '/authenticate': {
-            let data = ''
-            request.on('data', (chunk) => {
-                data += chunk
-            })
-            request.on('end', () => {
-                const params = parse(data)
-                response.end()
-            })
-            break
-        }
-    
-        default: {
-            response.writeHead(404, 'Something went wrong')
-            response.write("this page is not disponible or does not exists")
-            response.end()
-        }
-    }
+const server = express()
+
+
+server.get('/status', (_, response) => {
+    response.send({
+        status: 'OK'
+    })
 })
+
+server.post('/authenticate', express.json(), (request, response) => {
+    console.log(
+        "email:", request.body.email,
+        "password:", request.body.password
+    )
+    response.send()
+})
+
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1'
